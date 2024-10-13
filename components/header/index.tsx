2edfd45c0fa1cb7db09cc370f0ec9026230d285b
HieuNguyen67 <hieu012846@gmail.com>
2024-10-14 02:02:18 +0700

    fix backdrop

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -46,7 +46,7 @@ const Header: React.FC = () => {
   };
 
   return (
-    <nav className="bg-bluee sticky top-0 z-20">
+    <nav className="bg-bluee sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-14">
           <div className="flex items-center">
